f98e3c4dc5b0a8bb6abd92e9adfce10a9ce92616
malvi50 <malvi50@nmsu.edu>
2021-04-09 15:04:30 -0600

    Initial Project GIT Commit

---
+++ FinalProjectVirtualEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Michael Alvillar\Documents\ET458\CSchaferFinalProject\FinalProject\FinalProjectVirtualEnv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
